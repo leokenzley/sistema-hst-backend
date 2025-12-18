ef8f7e0d14571d6ced919d697f9d182ae0905459
Leo Kenzley <leo.kenzley@iteris.comm.br>
2025-12-18 18:47:11 -0300

    feat: criando o projeto

---
+++ src/main/java/br/com/thstec/hsts/HstsApplication.java
@@ -0,0 +1,13 @@
+package br.com.thstec.hsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thstec/hsts/HstsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thstec.hsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
