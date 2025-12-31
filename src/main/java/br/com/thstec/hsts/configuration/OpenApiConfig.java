b4fea63322c6f3ef37b7da559e05a59226230819
Leo Kenzley <leo.kenzley@iteris.comm.br>
2025-12-31 12:36:24 -0300

    feat: adicionando o nome do projeto e o número da sprint

---
+++ src/main/java/br/com/thstec/hsts/configuration/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package br.com.thstec.hsts.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("THS - HSTs")
+                        .description("API de gestão de HSTs")
+                        .version("v1.0.0"));
+    }
+}
--- src/main/java/br/com/thstec/hsts/relatorio/impl/SprintReportImpl.java
+++ src/main/java/br/com/thstec/hsts/relatorio/impl/SprintReportImpl.java
@@ -1,6 +1,12 @@
 package br.com.thstec.hsts.relatorio.impl;
 
+import br.com.thstec.hsts.entities.ProjetoEntity;
+import br.com.thstec.hsts.entities.SprintEntity;
+import br.com.thstec.hsts.exceptions.commons.NotFoundException;
 import br.com.thstec.hsts.relatorio.SprintReport;
+import br.com.thstec.hsts.repositories.ProjetoRepository;
+import br.com.thstec.hsts.repositories.SprintRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import net.sf.jasperreports.engine.JasperReport;
@@ -10,9 +16,14 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+@RequiredArgsConstructor
 @Component
 public class SprintReportImpl implements SprintReport {
+    private final SprintRepository sprintRepository;
+    private final ProjetoRepository projetoRepository;
     @Override
     public byte[] gerarPdf(Long id) throws Exception {
 
@@ -23,11 +34,21 @@ public class SprintReportImpl implements SprintReport {
         JasperReport report =
                 (JasperReport) JRLoader.loadObject(jasperStream);
 
+        SprintEntity sprint = sprintRepository.findById(id).orElseThrow(()-> new NotFoundException("Sprint não encontrada"));
+        ProjetoEntity projeto = projetoRepository.findById(sprint.getProjeto().getId()).orElseThrow(() -> new NotFoundException("Projeto não encontrado"));
+        Map<String, Object> params = new HashMap<>();
+
+        InputStream logoStream =
+                new ClassPathResource("report/images/logo.png").getInputStream();
+
+        params.put("LOGO", logoStream);
+        params.put("SPRINT_NUMERO", sprint.getNumero());
+        params.put("PROJETO_NOME", projeto.getNome());
 
         JasperPrint print =
                 JasperFillManager.fillReport(
                         report,
-                        null,
+                        params,
                         new JREmptyDataSource()
                 );
 
